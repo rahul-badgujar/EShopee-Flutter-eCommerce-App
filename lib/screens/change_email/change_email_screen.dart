df245d3876bcd5e26efe6ab1c8d50724289c78b2
Wahab Saheed Oluwatoyin <swahab947@gmail.com>
2023-04-22 18:37:08 +0100

    another commit

--- lib/screens/change_email/change_email_screen.dart
+++ lib/screens/change_email/change_email_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'components/body.dart';
 
 class ChangeEmailScreen extends StatelessWidget {
+  // ignore: todo
   // TODO: setState being called before build
   @override
   Widget build(BuildContext context) {
